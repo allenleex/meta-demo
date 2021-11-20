284e219808cfae156b269f8d24ad9e13e2de2dda
allenleex <allenleexxx@163.com>
2021-11-20 09:22:15 +0800

    sass

--- src/App.vue
+++ src/App.vue
@@ -13,5 +13,12 @@
 </script>
 
 <style>
-	/*每个页面公共css */
+/*每个页面公共css uParse为优化版本*/
+@import './common/app.css';
+@import './common/app-extend.css';
+@import './common/custom.css';
+@import './static/style/thorui-extend.css';
+/* #ifndef APP-NVUE */
+@import './components/uni/uParse/src/wxParse.css';
+/* #endif */
 </style>
